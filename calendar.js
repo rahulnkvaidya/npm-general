86ee9b45eabc6afd8dea50360d7db45a93aaa71f
Rahul V <rahulnkvaidya@yahoo.co.in>
2021-05-29 22:33:33 +0530

    calendar update

--- calendar.js
+++ calendar.js
@@ -3,11 +3,11 @@ const moment = require("moment");
 
 var calendar = {
      /**
-   * @param {data} data The number to raise.
+   * @param {dt} "2021-04-29" The number to raise.
    * @return {callback} callback raised to the n-th power.
    */
-  DateDay: function (date) {
-    const date = moment("2021-04-29"); // Thursday Feb 2015
+  DateDay: function (dt) {
+    const date = moment(dt); // Thursday Feb 2015
     const dow = date.day();
     const Days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
     return(Days[dow]);
